5fc30b8710dc362945be1768be7e8360b100975b
maximumleft <mdlevsha@gmail.com>
2025-06-21 15:53:05 +0300

    поправил миграции + добавил команду(пустышка)

---
+++ app/Console/Commands/TinkoffPortfolioSyncCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TinkoffPortfolioSyncCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:tinkoff-portfolio';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_06_21_120543_create_brokerage_account_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brokerage_account', function (Blueprint $table) {
+            $table->string('id')->unique();
+            $table->foreignId('user_id')->constrained('users', 'id')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brokerage_account');
+    }
+};
